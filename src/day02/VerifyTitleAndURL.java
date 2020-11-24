0a2bb8e83971196ce34281a95ba5ae1a77658476
Murat <murat.ozben1223@gmail.com>
2020-11-24 20:49:25 +0100

    2.ders eklendi

---
+++ src/day02/VerifyTitleAndURL.java
@@ -0,0 +1,43 @@
+package day02;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+
+public class VerifyTitleAndURL {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Casper\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+        driver.manage().window().maximize();
+
+        String actualResult = driver.getTitle();
+        String expectedResult = "google";
+
+        if (actualResult.equals(expectedResult)){
+            System.out.println("Page Title testi PASS");
+        }else{
+            System.out.println("Page Title testi FAILED");
+            System.out.println("Actual Page Title : Google ");
+        }
+
+
+        driver.navigate().to("https://youtube.com");
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "wwww.youtube.com";
+        if (actualURL.equals(expectedURL)){
+            System.out.println("URL testi Pass");
+        }else{
+            System.out.println("URL testi FAİLED");
+            System.out.println("Actual URL : https://youtube.com");
+        }
+        driver.close();
+
+
+    }
+}
